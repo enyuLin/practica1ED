8b7147a1a6ab9bbe0588bae3f675e97ac7efaaa5
enyulin <enyulin@192.168.1.115>
2017-03-05 02:05:23 +0100

    Ja funciona el programa, sobretot en el xifratge i desxifratge.

--- src/Aplicacio/Main.java
+++ src/Aplicacio/Main.java
@@ -11,90 +11,125 @@ public class Main {
 	public static void main(String[] args) throws CuaPlena, CuaBuida {
 		Scanner teclat = new Scanner(System.in);
 		TADCuaGenerica<Integer> cuaG = null;
-		TADCuaEnters cuaE;
-
+		TADCuaEnters cuaE = null;
+		int opcio = 0;
 		// Pregunntar a l'usuari les 4 diferents implementacions
-		System.out.println(
-				"Indiqui el tipus de Cua a utilitzar:\n\t1. Vector\n\t2. Circular \n\t3. Dinamica\n\t4. Estruc. de Java.Util.Collection");
-		int opcio = Integer.parseInt(teclat.nextLine().trim());
+		try {
+			System.out.println(
+					"Indiqui el tipus de Cua a utilitzar:\n\t1. Vector\n\t2. Circular \n\t3. Dinamica\n\t4. Estruc. de Java.Util.Collection");
+			opcio = Integer.parseInt(teclat.nextLine().trim());
+		} catch (NumberFormatException e) {
+			System.out.println("Format de l'opció incorrecte!");
+		}
 
-		
-		
 		// Preguntar a l'usuari el nom del fitxer
 		try {
 			System.out.println("Indiqui el nom del fitxer:");
-			String fileNom = teclat.nextLine().trim();
-			BufferedReader f = new BufferedReader(new FileReader(fileNom));
-			
+			String fitxerE = teclat.nextLine().trim();
+			BufferedReader f = new BufferedReader(new FileReader(fitxerE));
+
 			// Preguntar a l'usuari si es vol xifrar o desxifrar
 			System.out.println("Triï l'opció de xifratge:\n\t 1.Xifrar. \n\t 2.Desxifrar");
-			int opcioXifratge=0;
-			try{
+			int opcioXifratge = 0;
+			try {
 				opcioXifratge = Integer.parseInt(teclat.nextLine().trim());
-			} catch(NumberFormatException e){
+			} catch (NumberFormatException e) {
 				System.out.println("Format incorrecte de l'opció introduïda!");
 			}
-			
+
 			// Preguntar a l'usuari la clau a utilitzar
 			System.out.println("Indiqui la clau:");
 			String clau = teclat.nextLine().trim();
-			
+
 			if (opcio == 1) {
 				cuaE = new CuaEntVector(clau.length());
-				for (int i=0; i<clau.length();i++)
-					cuaE.encuar(clau.charAt(i)-'0');
-			} else if (opcio==2){
+				for (int i = 0; i < clau.length(); i++)
+					cuaE.encuar(clau.charAt(i) - '0');
+			} else if (opcio == 2) {
 				cuaE = new CuaEntCircular(clau.length());
-				for (int i=0; i<clau.length();i++)
-					cuaE.encuar(clau.charAt(i)-'0');
-			} else if (opcio== 3) {
+				for (int i = 0; i < clau.length(); i++)
+					cuaE.encuar(clau.charAt(i) - '0');
+			} else if (opcio == 3) {
 				cuaG = new CuaEntDinamica<Integer>();
-				for (int i=0; i<clau.length();i++)
-					cuaG.encuar(clau.charAt(i)-'0');
+				for (int i = 0; i < clau.length(); i++)
+					cuaG.encuar(clau.charAt(i) - '0');
 			} else {
 				// Opcio de java.util.collection
 			}
-			
-				
-			/*
-			try {
-				clau = Integer.parseInt(teclat.nextLine().trim());
-				//System.out.println("Clau:"+clau);
-			}catch(NumberFormatException e){
-				System.out.println("Format incorrecte de la clau introduïda!");
-			}
-			*/
-			
-			if (opcioXifratge==1){
-				
-			}else if(opcioXifratge==2){
-				
-			}
-			
-		
-			
-			
-			f.close();
-			
-		} catch (FileNotFoundException e){
-			System.out.println("El fitxer introduït no existeix");
-		} catch(IOException e){
-			System.out.println("S'ha produït un error general d'E/S");
-			
-		}
-		
-		
-		teclat.close();
-	}
 
-	// Mètode per comprovar que la clau introduïda per l'usuari sigui numèrica
-	public static boolean esValorNumeric(String valor) {
-		try {
-			Integer.parseInt(valor);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
+			String nomFitxer = fitxerE.substring(0, fitxerE.indexOf('.'));
+			BufferedWriter fitxerS;
+			if (opcioXifratge == 1) {
+				// Estem xifrant
+				try {
+					fitxerS = new BufferedWriter(new FileWriter(nomFitxer + "_vX.txt"));
+					String fraseE = f.readLine();
+					while (fraseE != null) {
+						String fraseS = "";
+						int desplaza = 0;
+						int valorE = 0;
+						for (int i = 0; i < fraseE.length(); i++) {
+							if (opcio == 1 || opcio == 2)
+								desplaza = cuaE.desencuar();
+							else if (opcio == 3)
+								desplaza = cuaG.desencuar();
+							valorE = (int) fraseE.charAt(i);
+							valorE += desplaza % 255;
+							fraseS += "" + (char) valorE;
+							if (opcio == 1 || opcio == 2)
+								cuaE.encuar(desplaza);
+							else if (opcio == 3)
+								cuaG.encuar(desplaza);
+						}
+						// System.out.println("fraseS: "+ fraseS);
+						fitxerS.write(fraseS);
+						fitxerS.newLine();
+						fraseE = f.readLine();
+					}
+					fitxerS.close();
+				} catch (IOException e) {
+					System.out.println("S'ha produit un error en el fitxer de sortida!");
+				}
+			} else if (opcioXifratge == 2) {
+				// Estem desxifrant
+				try {
+					fitxerS = new BufferedWriter(new FileWriter(nomFitxer + "_vD.txt"));
+					String fraseE = f.readLine();
+					while (fraseE != null) {
+						String fraseS = "";
+						int desplaza = 0;
+						int valorE = 0;
+						for (int i = 0; i < fraseE.length(); i++) {
+							if (opcio == 1 || opcio == 2)
+								desplaza = cuaE.desencuar();
+							else if (opcio == 3)
+								desplaza = cuaG.desencuar();
+							valorE = (int) fraseE.charAt(i);
+							valorE -= desplaza % 255;
+							fraseS += "" + (char) valorE;
+							if (opcio == 1 || opcio == 2)
+								cuaE.encuar(desplaza);
+							else if (opcio == 3)
+								cuaG.encuar(desplaza);
+						}
+						fitxerS.write(fraseS);
+						fitxerS.newLine();
+						fraseE = f.readLine();
+					}
+					fitxerS.close();
+
+				} catch (IOException e) {
+					System.out.println("S'ha produit un error en el fitxer de sortida!");
+				}
+			}
+			f.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("El fitxer introduït no existeix");
+		} catch (IOException e) {
+			System.out.println("S'ha produït un error general d'E/S");
+
 		}
+		teclat.close();
 	}
 
 }
