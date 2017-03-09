14559a40bdbf09a929d6f2a1137f25805b1b9a03
enyulin <enyulin@192.168.1.43>
2017-03-09 20:48:35 +0100

    Cifraje y descifraje solo con letras.

--- src/Aplicacio/Main.java
+++ src/Aplicacio/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		TADCuaEnters cuaE = null;
 		Queue<Integer> cuaCollection = new LinkedList<Integer>();
 		int opcio = 0;
-		
+
 		// Pregunntar a l'usuari les 4 diferents implementacions
 		try {
 			System.out.println(
@@ -56,7 +56,7 @@ public class Main {
 				cuaG = new CuaEntDinamica<Integer>();
 				for (int i = 0; i < clau.length(); i++)
 					cuaG.encuar(clau.charAt(i) - '0');
-			} else if (opcio== 4){
+			} else if (opcio == 4) {
 				// Opcio de java.util.collection
 				for (int i = 0; i < clau.length(); i++)
 					cuaCollection.add(clau.charAt(i) - '0');
@@ -65,34 +65,53 @@ public class Main {
 			String nomFitxer = fitxerE.substring(0, fitxerE.indexOf('.'));
 			BufferedWriter fitxerS = null;
 			try {
-				if (opcioXifratge==1) 
+				if (opcioXifratge == 1)
 					fitxerS = new BufferedWriter(new FileWriter(nomFitxer + "_vX.txt"));
-				else if (opcioXifratge==2)
+				else if (opcioXifratge == 2)
 					fitxerS = new BufferedWriter(new FileWriter(nomFitxer + "_vD.txt"));
 				String fraseE = f.readLine();
+
 				while (fraseE != null) {
 					String fraseS = "";
 					int desplaza = 0;
 					int valorE = 0;
+					char lletra;
 					for (int i = 0; i < fraseE.length(); i++) {
-						if (opcio == 1 || opcio == 2)
-							desplaza = cuaE.desencuar();
-						else if (opcio == 3)
-							desplaza = cuaG.desencuar();
-						else if (opcio == 4)
-							desplaza = cuaCollection.remove();
-						valorE = (int) fraseE.charAt(i);
-						if (opcioXifratge==1) 
-							valorE += desplaza % 255;
-						else if (opcioXifratge==2)
-							valorE -= desplaza % 255;
+						lletra = fraseE.charAt(i);
+						valorE = (int) lletra;
+						if (esLletraAlfabetic(lletra)) {
+							if (opcio == 1 || opcio == 2)
+								desplaza = cuaE.desencuar();
+							else if (opcio == 3)
+								desplaza = cuaG.desencuar();
+							else if (opcio == 4)
+								desplaza = cuaCollection.remove();
+
+							if (opcioXifratge == 1) {
+								if  (lletra >= 'A' && lletra <= 'Z') 
+									valorE = (int)((lletra - 'A' + desplaza) % 26 + 'A');
+								else 
+									valorE = (int)((lletra - 'a' + desplaza) % 26 + 'a');
+							} else if (opcioXifratge == 2) {
+								if  (lletra >= 'A' && lletra <= 'Z') {
+									valorE = (int)('A'+ (lletra - 'A' - desplaza) % 26);
+									if (valorE < 'A')
+										valorE += 26;
+								}else {
+									valorE = (int)('a' + (lletra - 'a' - desplaza) % 26);
+									if (valorE < 'a')
+										valorE += 26;
+								}
+							}
+
+							if (opcio == 1 || opcio == 2)
+								cuaE.encuar(desplaza);
+							else if (opcio == 3)
+								cuaG.encuar(desplaza);
+							else if (opcio == 4)
+								cuaCollection.add(desplaza);
+						}
 						fraseS += "" + (char) valorE;
-						if (opcio == 1 || opcio == 2)
-							cuaE.encuar(desplaza);
-						else if (opcio == 3)
-							cuaG.encuar(desplaza);
-						else if (opcio== 4)
-							cuaCollection.add(desplaza);
 					}
 					// System.out.println("fraseS: "+ fraseS);
 					fitxerS.write(fraseS);
@@ -111,6 +130,10 @@ public class Main {
 
 		}
 		teclat.close();
+	}
+
+	public static boolean esLletraAlfabetic(char c) {
+		return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 	}
 
 }
